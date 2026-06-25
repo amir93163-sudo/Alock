2ebbbf1604de5a2a07cee42c6f595f500a6556dd
Amir Ben Ari <  GitHub>
2026-06-25 23:52:23 +0300

    Remove invalid options route

--- server/app.js
+++ server/app.js
@@ -6,13 +6,13 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const customerRoutes = require("./routes/customerRoutes");
 const authRoutes = require("./routes/authRoutes");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 connectDB();
 
 app.use(cors());
-app.options("*", cors());
 app.use(express.json());
 
 app.use("/api/customers", customerRoutes);
@@ -25,7 +25,6 @@ app.get("/", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Server running on port ${PORT}`);
